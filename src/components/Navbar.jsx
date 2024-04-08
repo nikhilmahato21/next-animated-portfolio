c219def0707bcbac0268b722c4495d08f50202df
nikhil <nikhilmahato3337@gmail.com>
2024-04-08 07:56:06 +0530

    toggle menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,48 @@
+"use client";
 import Link from "next/link";
+import { useState } from "react";
+
+const links = [
+  { url: "/", title: "Home" },
+  { url: "/about", title: "About" },
+  { url: "/portfolio", title: "Portfolio" },
+  { url: "/contact", title: "Contact" },
+];
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
   return (
     <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
       <div className="">
-        <Link href='/' className="text-sm bg-black rounded-md p-1 font-semibold flex items-center justify-center">
-            <span className="text-white mr-1">nik</span>
-            <span className="w-12 h-7 rounded bg-white text-black flex items-center justify-center">.dev</span>
+        <Link
+          href="/"
+          className="text-sm bg-black rounded-md p-1 font-semibold flex items-center justify-center"
+        >
+          <span className="text-white mr-1">nik</span>
+          <span className="w-12 h-7 rounded bg-white text-black flex items-center justify-center">
+            .dev
+          </span>
         </Link>
-        
       </div>
       <div className="">
-        <button>
-            <div className="w-10"></div>
+        <button
+          className="w-10 h-8 flex flex-col justify-between z-50 relative"
+          onClick={() => setOpen(!open)}
+        >
+          <div className="w-10 h-1 bg-black rounded"></div>
+          <div className="w-10 h-1 bg-black rounded"></div>
+          <div className="w-10 h-1 bg-black rounded"></div>
         </button>
+        {/* MENU LIST */}
+        {open && (
+          <div className="absolute top-0 left-0 w-screen h-screen bg-black text-white flex flex-col items-center justify-center gap-8 text-4xl">
+            {links.map((link) => (
+              <Link href={link.url} key={link.title}>
+                {link.title}
+              </Link>
+            ))}
+          </div>
+        )}
       </div>
-      
     </div>
   );
 };

e9b279f15ddec597fe37c7d7d8fad3e6ac11b258
nikhil <nikhilmahato3337@gmail.com>
2024-04-09 16:07:04 +0530

    active link bg

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,16 @@
+import Image from "next/image";
+
 const Homepage = () => {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <Image
+        src="/avatar1.jpeg"
+        width={250}
+        height={200}
+        className="blob border border-black"
+      ></Image>
+    </div>
+  );
 };
 
 export default Homepage;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,8 @@
 "use client";
 import Link from "next/link";
+import Image from "next/image";
 import { useState } from "react";
+import NavLink from "./NavLInk";
 
 const links = [
   { url: "/", title: "Home" },
@@ -11,8 +13,16 @@ const links = [
 const Navbar = () => {
   const [open, setOpen] = useState(false);
   return (
-    <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
-      <div className="">
+    <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 text-xl">
+      {/* LINKS */}
+      <div className="hidden md:flex gap-4 w-1/3">
+        {links.map((link) => (
+          <NavLink link={link} key={link.title}/>
+           
+        ))}
+      </div>
+      {/* Logo */}
+      <div className="md:hidden lg:flex w-1/3 justify-center ">
         <Link
           href="/"
           className="text-sm bg-black rounded-md p-1 font-semibold flex items-center justify-center"
@@ -23,7 +33,30 @@ const Navbar = () => {
           </span>
         </Link>
       </div>
-      <div className="">
+      {/* Socials */}
+      <div className="hidden md:flex gap-4 w-1/3">
+        <Link href="/">
+          <Image src="/github.png" alt="github" width={24} height={24} />
+        </Link>
+        <Link href="/">
+          <Image src="/dribbble.png" alt="" width={24} height={24} />
+        </Link>
+        <Link href="/">
+          <Image src="/instagram.png" alt="" width={24} height={24} />
+        </Link>
+        <Link href="/">
+          <Image src="/facebook.png" alt="" width={24} height={24} />
+        </Link>
+        <Link href="/">
+          <Image src="/pinterest.png" alt="" width={24} height={24} />
+        </Link>
+        <Link href="/">
+          <Image src="/linkedin.png" alt="" width={24} height={24} />
+        </Link>
+      </div>
+
+      {/* RESPONSIVE MENU */}
+      <div className=" md:hidden">
         <button
           className="w-10 h-8 flex flex-col justify-between z-50 relative"
           onClick={() => setOpen(!open)}
